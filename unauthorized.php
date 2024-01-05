b786fd41022099c4949107da698d07aeaabeb953
StacikM <146170881+StacikM@users.noreply.github.com>
2024-01-05 20:58:38 +0100

    Update unauthorized.php

---
+++ unauthorized.php
@@ -0,0 +1,6 @@
+<?php
+// Display an unauthorized access message
+echo '<h1>Unauthorized Access</h1>';
+echo '<p>You do not have permission to access this page.</p>';
+echo '<a href="discord_logout.php">Logout</a>';
+?>
